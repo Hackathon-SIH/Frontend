48e43024700a8945de499219777fc9d84a5ab1a4
Rudraksh Gupta <rudrakshgupta40@gmail.com>
2024-11-22 06:16:42 +0530

    Sign Up Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,36 @@
 import './App.css';
+import React, { useState } from 'react';
 import FeaturedJobs from './components/FeaturedJobs/FeaturedJobs';
 import Footer from './components/footer/footer';
 import HowItWorks from './components/HowItWorks/HowItWorks';
 import Navbar from './components/Navbar/Navbar';
 import PopularCategory from './components/PopularCategory/PopularCategory';
+import SignUpForm from './components/SignUpForm/SignUpForm';
 import TopCompanies from './components/TopCompanies/TopCompanies';
 import LandingPage from './pages/LandingPage/LandingPage';
 
 function App() {
+  const [isSignUpVisible, setSignUpVisible] = useState(false);
+
+    const handleSignUpClick = () => {
+        setSignUpVisible(true);
+    };
+
+    const handleCloseSignUp = () => {
+        setSignUpVisible(false);
+    };
+
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar onSignUpClick={handleSignUpClick} />
       <LandingPage />
       <FeaturedJobs />
       <HowItWorks />
       <PopularCategory />
       <TopCompanies />
       <Footer />
+      <SignUpForm isVisible={isSignUpVisible} onClose={handleCloseSignUp} />
     </div>
   );
 }
